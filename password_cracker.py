01d27f27ac6f8900fa7acc78f4af071b1ea032c4
Aleksandr Safronenko <agsafronenko@gmail.com>
2024-10-31 08:13:59 +0000

    final

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,37 @@
 import hashlib
 
+
+def read(file):
+    with open(file, "r") as file:
+        return [line.strip() for line in file]
+
+
+def hash_pass(password):
+    return hashlib.sha1(password.encode()).hexdigest()
+
+
 def crack_sha1_hash(hash, use_salts = False):
+
+    for password in read("top-10000-passwords.txt"):
+        for salt in read("known-salts.txt"):
+            for salted_password in [password + salt, salt + password, password]:
+                if hash_pass(salted_password) == hash:
+                    return password
+
+    return "PASSWORD NOT IN DATABASE"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return True
